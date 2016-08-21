f590a84465e917bce5ce9f6454f2b2ab6a358354
Qiu ZhiChang <lijinlon@gmail.com>
2016-08-21 23:12:01 +0800

    linux-pcie 驱动，能够测速

---
+++ .pcie-lat.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/aqiu/Desktop/pcie-lat-master/pcie-lat.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/aqiu/Desktop/pcie-lat-master/pcie-lat.ko /home/aqiu/Desktop/pcie-lat-master/pcie-lat.o /home/aqiu/Desktop/pcie-lat-master/pcie-lat.mod.o
---
+++ .tmp_versions/pcie-lat.mod
@@ -0,0 +1,2 @@
+/home/aqiu/Desktop/pcie-lat-master/pcie-lat.ko
+/home/aqiu/Desktop/pcie-lat-master/pcie-lat.o
